9945793e38bbc9a67799f6e5eea694b03bbd0bb5
trunkey2003 <21521049@gm.uit.edu.vn>
2022-06-11 16:55:35 +0700

    add react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log("Hello");
+
+function Example() {
+
+    return (
+        <div className='text-[100px] pb-5 text-red-300'>Hello Worldd</div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
+};
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/user', function () {
+    $user = new User();
+    $allUser = $user::all(); 
+    // dd($allUser);
+    return $allUser;
 });
